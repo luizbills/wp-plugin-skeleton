4a11504340cf5a68fba2ede1e9cafd2476b76c4d
Luiz Paulo "Bills <luizpbills@gmail.com>
2019-06-12 12:32:01 -0300

    Update debug.php

--- src/core/_helpers/debug.php
+++ src/core/_helpers/debug.php
@@ -13,5 +13,5 @@ function throw_if ( $condition, $message, $code = 0 ) {
 function log ( ...$args ) {
 	$is_enabled = \apply_filters( prefix( 'debug_log_enabled' ), get_defined( 'WP_DEBUG_LOG', false ) );
 	if ( ! $is_enabled ) return;
-	\error_log( config_get( 'SLUG' ) . ': ' . format( $args ) );
+	\error_log( config_get( 'SLUG' ) . ': ' . format( ...$args ) );
 }
