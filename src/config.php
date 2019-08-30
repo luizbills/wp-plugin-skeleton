3a576404fff56ae1ed6322839a39da45a05c9261
Luiz Paulo "Bills <luizbills@pm.me>
2019-08-30 11:42:14 -0300

    remove name and version references

--- src/config.php
+++ src/config.php
@@ -2,12 +2,10 @@
 
 // all values here can be acessed with `config_get` helper
 return [
-	'NAME' => '{{plugin_name}}',
-	'VERSION' => '{{plugin_version}}',
 	'ASSETS_DIR' => 'assets',
 	'TEMPLATES_DIR' => 'templates',
 	'LANGUAGES_DIR' => 'languages'
 ];
 
 // important: don't use reserved keys (they're used internally)
-// reserved keys = SLUG, PREFIX, MAIN_FILE, ROOT_DIR
+// reserved keys = NAME, VERSION, SLUG, PREFIX, MAIN_FILE, ROOT_DIR
