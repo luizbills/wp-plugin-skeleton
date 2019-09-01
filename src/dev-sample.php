e7d8e3d6e9759664f2127ead9e7953e0775aa728
Luiz Bills <luizpbills@gmail.com>
2019-09-01 00:41:44 -0300

    improve dev-sample.php

--- src/dev-sample.php
+++ src/dev-sample.php
@@ -1,5 +1,19 @@
 <?php
 
+namespace src_namespace__;
+
 use src_namespace__\functions as h;
 
 // test and simulate things here
+class Test {
+	public function boot () {
+		// some code here
+	}
+
+	public function init () {
+		// or here
+	}
+}
+
+h\load_class( Test::class, 'boot' );
+h\load_class( Test::class, 'init' );
