b52e4ed57b90676795b5a64acfc79fd219a259d6
Luiz Bills <luizpbills@gmail.com>
2019-08-31 23:03:00 -0300

    renamed core folders

--- src/core/load_helpers.php
+++ src/core/load_helpers.php
@@ -2,17 +2,11 @@
 
 use src_namespace__\functions as h;
 
-$dir = __DIR__ . '/_helpers';
-$file_helper = "$dir/file.php";
+$dir = __DIR__ . '/functions';
 
 // load the 'rscandir' function first
-include_once $file_helper;
+include_once "$dir/file.php";
 
-$files = h\rscandir( $dir );
-
-// don't load file.php again
-unset( $files[ array_search( $file_helper, $files, true ) ] );
-
-foreach ( $files as $file ) {
+foreach ( h\rscandir( $dir ) as $file ) {
 	include $file;
 }
