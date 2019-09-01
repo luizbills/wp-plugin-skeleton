b9c6821c2b8dc6c30300cd3a706e3f8d03dc7ee6
Luiz Paulo "Bills <luizbills@pm.me>
2019-08-31 22:51:48 -0300

    Update callback.php

--- src/core/_helpers/callback.php
+++ src/core/_helpers/callback.php
@@ -8,23 +8,9 @@ function return_value ( $value ) {
 	};
 }
 
-function return_value_if ( $condition, $value ) {
-	return function ( $v ) use ( $condition, $value ) {
-		if ( $condition ) return $value;
-		return $v;
-	};
-}
-
 function return_push_value ( $value ) {
 	return function ( $arr ) use ( $value ) {
 		$arr[] = $value;
-		return $arr;
-	};
-}
-
-function return_push_value_if ( $condition, $value ) {
-	return function ( $arr ) use ( $condition, $value ) {
-		if ( $condition ) return $arr[] = $value;
 		return $arr;
 	};
 }
