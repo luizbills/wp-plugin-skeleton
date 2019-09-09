b4d9248e2f7042ce6452b703331da5d52f74608b
Luiz Paulo "Bills <luizbills@pm.me>
2019-09-09 19:44:37 -0300

    Update main.php

--- src/main.php
+++ src/main.php
@@ -20,7 +20,17 @@ require_once __DIR__ . '/core/load_helpers.php';
 
 use src_namespace__\functions as h;
 
-if ( ! h\get_defined( 'DOING_AJAX' ) && h\get_defined( 'WP_DEBUG' ) && h\get_defined( 'WP_DEBUG_DISPLAY' ) ) {
+$use_whoops = h\all_equal(
+	[
+		class_exists( 'Whoops\\Run' ),
+		! h\get_defined( 'DOING_AJAX' ),
+		h\get_defined( 'WP_DEBUG' ),
+		h\get_defined( 'WP_DEBUG_DISPLAY' )
+	],
+	true,
+	true
+);
+if ( $use_whoops ) {
 	$whoops = h\config_set( 'whoops_instance', new \Whoops\Run() );
 	$whoops->pushHandler( new \Whoops\Handler\PrettyPageHandler() );
 	$whoops->register();
