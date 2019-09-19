e42e9694f6be1597df97d4ba0c659f6bfe4c13a2
Luiz Paulo "Bills <luizbills@pm.me>
2019-09-19 12:51:23 -0300

    Create notify.php

---
+++ src/core/functions/notify.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace src_namespace__\functions;
+
+function notify ( $message, $options = [] ) {
+	$options['message'] = $message;
+
+	if ( \apply_filters( prefix( 'notify_email_enabled' ), true ) ) {
+		$recipients = [ \get_bloginfo( 'admin_email' ) ];
+		$subject = config_get( 'NAME' ) . ' Notification';
+		$headers = [];
+		$email_content_type = array_get( $options, 'email_content_type', 'content-type: text/html; charset=utf-8' );
+
+		$headers[] = $email_content_type;
+
+		\wp_mail(
+			\apply_filters( prefix( 'notify_email_recipients' ), $recipients, $options ),
+			\apply_filters( prefix( 'notify_email_subject' ), $subject, $options ),
+			\apply_filters( prefix( 'notify_email_message' ), $options['message'], $options ),
+			\apply_filters( prefix( 'notify_email_headers' ), $headers, $options )
+		);
+	}
+
+	\do_action( prefix( 'notify' ), $options );
+	log( "Notification sent: ${options['message']}" );
+}
