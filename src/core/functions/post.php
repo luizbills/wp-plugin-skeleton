136d0d676f61790c1747d0ec926098cddfee639a
Luiz Paulo "Bills <luizbills@pm.me>
2019-09-23 07:50:21 -0300

    Update post.php

--- src/core/functions/post.php
+++ src/core/functions/post.php
@@ -4,8 +4,10 @@ namespace src_namespace__\functions;
 
 function get_post ( $id, $post_type = 'post' ) {
 	$post = \get_post( \intval( $id ) );
-	if ( $post && $post_type === $post->post_type ) {
-		return $post;
-	}
-	return false;
+
+	throw_if( null === $post, "Invalid Post ID: $id" );
+	throw_if( $post_type !== $post->post_type, "Post #$id isn't a '$post_type'" );
+
+	return $post;
 }
+
