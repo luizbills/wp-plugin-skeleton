55738c4bd0a1527ca23bb301f5a8c76147337829
Luiz Paulo "Bills <luizbills@pm.me>
2019-11-05 09:27:44 -0300

    Update load.php

--- src/load.php
+++ src/load.php
@@ -4,63 +4,41 @@ namespace src_namespace__;
 
 use src_namespace__\functions as h;
 
-$init_classes = [
-	// classes with `init` method
-	// init() method is called in 'plugins_loaded' hook
-	// just put the class name in this array
-	// to set a priority use an array
-	// e.g.: [ Namespace\MyClass::class, 20 ] // priority = 20
-	//Some_Namespace\Foo::class,
+// ADD YOUR CLASSES TO BE DINAMICALLY LOADED
+$classes_to_load = [
+	Plugin_Dependencies::class,
 ];
 
-$boot_classes = [
-	// classes with `boot` method
-	// boot() method is called in 'init' hook
-	//Some_Namespace\Foo::class,
-	Plugin_Dependencies::class
-];
+// DON'T EDIT BELOW
+$methods = h\get_load_class_methods();
 
-$pre_boot_classes = [
-	// classes with `pre_boot` method
-	//Some_Namespace\Foo::class,
-];
-
-$activation_classes = [
-	// classes with 'activation' static method
-	//Some_Namespace\Foo::class,
-];
-
-$deactivation_classes = [
-	// classes with 'deactivation' static method
-	//Some_Namespace\Foo::class,
-];
-
-// DON'T EDIT HERE
-$classes = [
-	'pre_boot' => $pre_boot_classes,
-	'boot' => $boot_classes,
-	'init' => $init_classes,
-];
-
-foreach ( $classes as $context => $class_names ) {
-	foreach ( $class_names as $class_name ) {
+foreach ( $classes_to_load as $class_name ) {
+	foreach ( $methods as $method ) {
 		$priority = 10;
-		if ( is_array( $class_name ) ) {
-			$priority = $class_name[1];
+
+		if ( \is_array( $class_name ) ) {
 			$class_name = $class_name[0];
+			$priority = $class_name[1];
 		}
-		h\load_class( $class_name, $context, $priority );
+
+		if ( method_exists( $class_name, $method ) ) {
+			h\load_class( $class_name, $method, $priority );
+		}
 	}
 }
 
-\register_activation_hook( h\config_get( 'MAIN_FILE' ), function () use ( $activation_classes ) {
-	foreach ( $activation_classes as $class_name ) {
-		echo call_user_func( [ $class_name, 'activation' ] );
+\register_activation_hook( h\config_get( 'MAIN_FILE' ), function () use ( $classes_to_load ) {
+	foreach ( $classes_to_load as $class_name ) {
+		if ( \method_exists( $class_name, 'activation' ) ) {
+			\call_user_func( [ $class_name, 'activation' ] );
+		}
 	}
 } );
 
-\register_deactivation_hook( h\config_get( 'MAIN_FILE' ), function () use ( $deactivation_classes ) {
-	foreach ( $deactivation_classes as $class_name ) {
-		echo call_user_func( [ $class_name, 'deactivation' ] );
+\register_deactivation_hook( h\config_get( 'MAIN_FILE' ), function () use ( $classes_to_load ) {
+	foreach ( $classes_to_load as $class_name ) {
+		if ( \method_exists( $class_name, 'deactivation' ) ) {
+			\call_user_func( [ $class_name, 'deactivation' ] );
+		}
 	}
 } );
