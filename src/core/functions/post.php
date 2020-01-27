a7132c3258c52e1a82e765a4de385d8ab4698539
Luiz "Bills <luizbills@pm.me>
2020-01-27 10:48:00 -0300

    Update post.php

--- src/core/functions/post.php
+++ src/core/functions/post.php
@@ -2,12 +2,11 @@
 
 namespace src_namespace__\functions;
 
-function get_post ( $id, $post_type = 'post' ) {
-	$post = \get_post( \intval( $id ) );
+function get_post_by_type ( $id, $post_type = 'post' ) {
+	$post = \get_post( (int) $id );
 
 	throw_if( null === $post, "Invalid post ID: $id", 'invalid-post-id' );
 	throw_if( $post_type !== $post->post_type, "Invalid type for post #$id: $post_type", 'invalid-post-type' );
 
 	return $post;
 }
-
