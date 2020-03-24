09e6a8673fb1bcbd9a12628a57f3b70babe96c13
Luiz "Bills <luizbills@pm.me>
2020-03-24 09:40:21 -0300

    Update notify.php

--- src/core/functions/notify.php
+++ src/core/functions/notify.php
@@ -2,28 +2,56 @@
 
 namespace src_namespace__\functions;
 
+/*
+	USAGE:
+	
+	notify( 'your message', [
+		'email' => [
+			'recipients' => 'dest@email.com',
+			'subject'    => 'your subject',
+			'to_admin'   => true, // send email to site admin
+			'handler'    => function ( $mes, $opts ) {
+				// to use another mailer
+				// omit this option to use wp_mail
+			}
+		],
+		'sms' => [
+			// implement this notification channel
+			// handle it with 'handler' option here
+			// or listening the action hook '{$prefix}handle_notification_sms'
+		]
+	);
+*/
 function notify ( $message, $options = [] ) {
 	// default handler
 	if ( isset( $options['email'] ) ) {
 		$opts = array_get( $options, 'email', [] );
 
-		$recipients    = array_get( $opts, 'recipients', [] );
-		$subject       = array_get( $opts, 'subject', config_get( 'NAME' ) . ' Notification' );
-		$content_type  = array_get( $opts, 'content_type', 'content-type: text/html; charset=utf-8' );
+		$opts = \apply_filters( prefix( 'notify_email_options' ), $opts );
+		$content = \apply_filters( prefix( 'notify_email_content' ), $message, $opts );
+
+		$opts['recipients']  = array_get( $opts, 'recipients', [] );
+		$opts['subject']     = array_get( $opts, 'subject', config_get( 'NAME' ) . ' Notification' );
+		$opts['content_type']     = array_get( $opts, 'content_type', 'content-type: text/html; charset=utf-8' );
+		$opts['headers']     = array_get( $opts, 'headers', [] );
+		$opts['attachments'] = array_get( $opts, 'attachments', [] );
+
+		$opts['headers'][] = $opts['content_type'];
 
 		// notify site admin by default
 		if ( array_get( $opts, 'to_admin', true ) ) {
-			$recipients[] = \get_bloginfo( 'admin_email' );
+			$opts['recipients'][] = \get_bloginfo( 'admin_email' );
 		}
 
 		if ( isset( $opts['handler'] ) && is_callable( $opts['handler'] ) ) {
-			call_user_func( $opts['handler'], $message, $opts );
+			call_user_func( $opts['handler'], $content, $opts );
 		} else {
 			\wp_mail(
-				\apply_filters( prefix( 'notify_email_recipients' ), $recipients, $opts ),
-				\apply_filters( prefix( 'notify_email_subject' ), $subject, $opts ),
-				\apply_filters( prefix( 'notify_email_message' ), $message, $opts ),
-				\apply_filters( prefix( 'notify_email_headers' ), $headers, $opts )
+				$opts['recipients'],
+				$opts['subject'],
+				$content,
+				$opts['headers'],
+				$opts['attachments']
 			);
 		}
 	}
