b2ba6af64c3ed49f40205ce49bdb4835b46a2a56
Luiz "Bills <luizpbills@gmail.com>
2020-12-10 08:17:25 -0300

    Update Cron.php

--- snippets/Cron.php
+++ snippets/Cron.php
@@ -20,13 +20,17 @@ final class Schedule_Event {
 		// $this->add_filter( 'cron_schedules', 'add_cron_interval' );
 		$schedule_action = h\prefix( self::ACTION );
 		if ( ! \wp_next_scheduled( $schedule_action ) ) {
-			\wp_schedule_event( time(), 'hourly', $schedule_action );
+			\wp_schedule_event( time(), $this->get_recurrence(), $schedule_action );
 		}
 	}
 
 	public function __init () {
 		$schedule_action = h\prefix( self::ACTION );
 		$this->add_action( $schedule_action, 'callback' );
+	}
+	
+	public function get_recurrence () {
+		return ''hourly';
 	}
 
 	public function callback () {
