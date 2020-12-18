e6b06bea628c38ec5a3d9af459e33dcb6e4e10b2
Luiz "Bills <luizbills@pm.me>
2020-12-18 08:42:02 -0300

    Update admin-notice.php

--- src/templates/admin-notice.php
+++ src/templates/admin-notice.php
@@ -1,7 +1,4 @@
-<?php
-use src_namespace__\functions as h;
-h\array_ensure_keys( $var, [ 'class', 'message' ] ); 
-?>
+<?php // docs: https://developer.wordpress.org/reference/hooks/admin_notices/ ?>
 <div class="notice <?= v( $var['class'] ) ?>">
 	<p><?= v( $var['message'], 'default("Missing Message")', 'safe_html', 'raw' ) ?></p>
 </div>
