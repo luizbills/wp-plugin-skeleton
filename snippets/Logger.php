2b8f490db098a72e8dbbf4fa25716cc01afcdcc4
Luiz "Bills <luizbills@pm.me>
2021-02-09 19:02:59 -0300

    Update Logger.php

--- snippets/Logger.php
+++ snippets/Logger.php
@@ -10,30 +10,31 @@ use Monolog\Formatter\LineFormatter;
 // requirements: composer require monolog/monolog
 // usage: https://github.com/Seldaek/monolog/blob/2.2.0/doc/01-usage.md
 final class Logger {
-	protected static $logger = null;
+	protected static $loggers = [];
 
 	public static function open ( $name = 'debug' ) {
-		if ( null === self::$logger ) {
+		if ( ! isset( self::$loggers[ $name ] ) ) {
 			try {
 				$format = "[%datetime%] %level_name% %message% %context% %extra%\n";
 				$formatter = new LineFormatter( $format );
 				$logger = new MonologLogger( $name );
 				$maxFiles = \apply_filters( h\prefix( 'rotating_logs_max_files' ), 7, $name );
+				$debug_log = \apply_filters( h\prefix( 'debug_log' ), h\get_defined( 'WP_DEBUG' ), $name );
+				$filename = h\config_get( 'ROOT_DIR' ) . '/' . h\config_get( 'LOGS_DIR', 'logs' ) . "/$name.log";
 				$handler = new RotatingFileHandler(
-					h\config_get( 'ROOT_DIR' ) . "/logs/$name.log",
+					$filename,
 					$maxFiles,
-					h\get_defined( 'WP_DEBUG' ) ? MonologLogger::DEBUG : MonologLogger::ERROR
+					$debug_log ? MonologLogger::DEBUG : MonologLogger::ERROR
 				);
 
 				$handler->setFormatter( $formatter );
 				$logger->pushHandler( $handler );
 
-				self::$logger = \apply_filters( h\prefix( 'get_logger' ), $logger, $name );
+				self::$loggers[ $name ] = \apply_filters( h\prefix( 'get_logger' ), $logger, $name );
 			} catch ( \Throwable $e ) {
 				h\logf( 'Can\'t initialize Monolog\\Logger class: ' . $e->getMessage() );
 			}
 		}
-
-		return self::$logger;
+		return self::$loggers[ $name ];
 	}
 }
