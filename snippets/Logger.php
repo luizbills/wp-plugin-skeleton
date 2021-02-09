835ab9c6e017b047cee2b74c6a6377cd8eb37e54
Luiz "Bills <luizbills@pm.me>
2021-02-09 16:25:56 -0300

    Update Logger.php

--- snippets/Logger.php
+++ snippets/Logger.php
@@ -9,20 +9,28 @@ use Monolog\Handler\RotatingFileHandler;
 // requirements: composer require monolog/monolog
 // usage: https://github.com/Seldaek/monolog/blob/2.2.0/doc/01-usage.md#adding-extra-data-in-the-records
 final class Logger {
+	protected static $logger = null;
 
-	public static function open ( $filename = 'debug' ) {
-		try {
-			$logger = new MonologLogger( $filename );
-			$logger->pushHandler(
-				new RotatingFileHandler(
-					h\config_get( 'ROOT_DIR' ) . "/logs/$filename.log",
+	public static function open ( $name = 'debug' ) {
+		if ( null === self::$logger ) {
+			try {
+				$format = "[%datetime%] %level_name% %message% %context% %extra%\n";
+				$formatter = new LineFormatter( $format );
+				$logger = new MonologLogger( $name );
+				$handler = new RotatingFileHandler(
+					h\config_get( 'ROOT_DIR' ) . "/logs/$name.log",
 					\WP_DEBUG ? MonologLogger::DEBUG : MonologLogger::WARNING
-				)
-			);
-			return $logger;
-		} catch ( \Throwable $e ) {
-			h\logf( 'Error when trying to use Logger class: ' . $e->getMessage() );
+				);
+
+				$handler->setFormatter( $formatter );
+				$logger->pushHandler( $handler );
+
+				self::$logger = \apply_filters( h\prefix( 'get_logger' ), $logger, $name );
+			} catch ( \Throwable $e ) {
+				h\logf( 'Can\'t initialize Monolog\\Logger class: ' . $e->getMessage() );
+			}
 		}
+
+		return self::$logger;
 	}
 }
-
