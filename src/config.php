1338b55b922e7dc7577f06b7465266f034019466
Luiz "Bills <luizbills@pm.me>
2021-02-24 10:17:39 -0300

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,12 +1,11 @@
 <?php
 // Notes: 
-//   * Reserved keys = NAME, VERSION, MAIN_FILE, ROOT_DIR, NAMESPACE_BASE
-//   * Don't use reserved keys (they're used internally).
-//
-//   * The SLUG and PREFIX keys are auto-generated (based in plugin name), if you do not set them here.
-//   * Don't declare any key starting with underline 
+//  * Don't use reserved keys (they're used internally). 
+//  * Reserved keys = NAME, VERSION, MAIN_FILE, ROOT_DIR, NAMESPACE_BASE
+//  * The SLUG and PREFIX keys are auto-generated (based in plugin name), if you do not set them here.
+//  * Don't declare any key starting with underline 
+//  * all values here can be acessed with `h\config_get` helper
 
-// all values here can be acessed with `h\config_get` helper
 return [
 	'ASSETS_DIR' => 'assets',
 	'TEMPLATES_DIR' => 'templates',
@@ -17,5 +16,3 @@ return [
 	// disable the plugin cache while in development or debugging
 	'DISABLE_CACHE' => WP_DEBUG,
 ];
-
-
