73944138c99664f061925360d4cad5cd2e3fe354
Luiz "Bills <luizbills@pm.me>
2021-02-25 17:41:21 -0300

    Update debug.php

--- src/core/functions/debug.php
+++ src/core/functions/debug.php
@@ -2,14 +2,13 @@
 
 namespace src_namespace__\functions;
 
-function throw_if ( $condition, $message, $error_id = '' ) {
+function throw_if ( $condition, $message, $error_code = 500 ) {
 	if ( $condition ) {
 		if ( \is_callable( $message ) ) {
 			$message = $message();
 		}
 		$slug = config_get( 'SLUG' );
-		$error_slug = $error_id ? "[$slug-error-$error_id]" : "[$slug-error]";
-		throw new \ErrorException( "$error_slug $message" );
+		throw new \RuntimeException( "[$slug-error] $message", (int) $error_code );
 	}
 	return $condition;
 }
@@ -17,36 +16,17 @@ function throw_if ( $condition, $message, $error_id = '' ) {
 function handle_exception ( \Throwable $exception, $callback = null ) {
 	$message = $exception->getMessage();
 	$slug = config_get( 'SLUG' );
-	$error_prefix = "[$slug-error";
+	$error_prefix = "[$slug-error]";
 
 	if ( str_starts_with( $message, $error_prefix ) ) {
-		$glue = ']';
-		$parts = \explode( $glue, $message );
-
-		// get error id
-		$pattern = '/^' . \preg_quote( $error_prefix, '/' ) . '-?/';
-		$error_id = \preg_replace( $pattern, '', \array_shift( $parts ) );
-
-		// get message
-		$error_message = trim( \implode( $glue, $parts ) );
-
-		// handle
-		$http_code = is_callable( $callback)
-			? $callback( trim( $error_message ), trim( $error_id ) )
-			: false;
-
+		$error_message = trim( str_after( $message, $error_prefix ) );
 		return [
 			'error_message' => $error_message,
-			'error_id'      => $error_id,
-			'code'          => $http_code ? intval( $http_code ) : 400,
+			'error_code' => $exception->getCode(),
 		];
 	}
 
-	return [
-		'error_message' => $exception->getMessage(),
-		'error_id'      => null,
-		'code'          => 500,
-	];
+	return false;
 }
 
 function logf ( ...$args ) {
