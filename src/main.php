7a6fc76dfa11d9b8bffa4a64c8ebec5f1cd2b6e3
Luiz "Bills <luizbills@pm.me>
2021-09-16 11:11:39 -0300

    Update main.php

--- src/main.php
+++ src/main.php
@@ -16,8 +16,6 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
 if ( ! defined( 'WPINC' ) ) die();
 
-require_once __DIR__ . '/vendor/autoload.php';
-
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require_once __DIR__ . '/vendor/autoload.php';
 	\src_namespace__\Core\Plugin::run( __FILE__ );
