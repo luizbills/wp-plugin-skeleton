101d719416216e6cd35407ee9741e355d7a6a333
Luiz "Bills <luizbills@pm.me>
2022-03-23 10:11:56 -0300

    Update Cron.php

--- snippets/Cron.php
+++ snippets/Cron.php
@@ -13,33 +13,27 @@ final class Cron {
 	}
 
 	public static function __deactivation () {
-		$cron_action = h\prefix( self::get_action() );
+		$cron_action = self::get_action();
 		$timestamp = \wp_next_scheduled( $cron_action );
 		\wp_unschedule_event( $timestamp, $cron_action );
 	}
 
 	public function __boot () {
 		// $this->add_filter( 'cron_schedules', 'add_cron_interval' );
-		
-		$cron_action = h\prefix( self::get_action() );
+		$cron_action = self::get_action();
 		if ( ! \wp_next_scheduled( $cron_action ) ) {
 			\wp_schedule_event( time(), $this->get_recurrence(), $cron_action );
 		}
 	}
-
+	
 	public function __init () {
-		$cron_action = h\prefix( self::get_action() );
-		$this->add_action( $cron_action, 'callback' );
+		$this->add_action( self::get_action(), 'callback' );
 	}
 	
-	public function callback () {
-		// TODO
-	}
-
 	public function get_recurrence () {
 		return 'hourly';
 	}
-
+	
 	// public function add_cron_interval ( $schedules ) {
 	// 	$schedules[ 'every_minute' ] = [
 	// 		'interval' => 1 * \MINUTE_IN_SECONDS,
@@ -47,4 +41,8 @@ final class Cron {
 	// 	];
 	// 	return $schedules;
 	// }
+	
+	public function callback () {
+		h\logf( 'Triggered cron action' );
+	}
 }
